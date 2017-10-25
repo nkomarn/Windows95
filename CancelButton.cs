f3524238df06f40448d19930300cbaaccfa8ef7d
TechToolbox <nkomarn@hotmail.com>
2017-10-25 14:09:47 -0700

    Add files via upload

---
+++ CancelButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CancelButton : MonoBehaviour {
+	public Button cancelBUTTON;
+	public GameObject window;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		cancelBUTTON.onClick.AddListener (cancel);
+	}
+	void cancel(){
+		window.SetActive (false);
+	
+	}
+}
