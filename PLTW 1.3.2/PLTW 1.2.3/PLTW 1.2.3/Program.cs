77cbb13e5860eb4f68b8b04d042dcd77f1e7a62a
Jacob Cunningham <jacob.joc@gmail.com>
2017-11-01 14:52:36 -0700

    Add files via upload

---
+++ PLTW 1.3.2/PLTW 1.2.3/PLTW 1.2.3/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace PLTW
+{
+    public class DataTypes
+    {
+        //example of types
+        public static int MyInteger;
+        public static float MyFloat;
+        public static double MyDouble;
+        public static string MyString;
+        public static bool MyBoolean;
+
+      
+        //setting values of types
+        public static void Integer()
+        {
+            MyInteger = 1;
+        }
+
+        public static void Float()
+        {
+            MyFloat = 0.1f;
+        }
+
+        public static void Double()
+        {
+            MyDouble = 0.001;
+        }
+
+        public static void String()
+        {
+            MyString = "string";
+        }
+
+        public static void Boolean()
+        {
+            MyBoolean = true;
+        }
+
+        //types 2
+        public static int ValueInteger;
+        public static float ValueFloat;
+        public static double ValueDouble;
+        public static string ValueString;
+        public static bool ValueBoolean;
+        
+        //function that uses/sets types 2
+        public static void Calculator()
+        {
+            if (MyBoolean)
+            {
+                ValueInteger = MyInteger + MyInteger;
+                ValueFloat = MyFloat + MyFloat;
+                ValueDouble = MyDouble + MyDouble;
+            }
+            if (ValueInteger == MyInteger * 2)
+            {
+                ValueString = MyString +" " + MyString;
+            }
+            if (ValueString == "string" +" " + "string")
+            {
+                ValueBoolean = false;
+            }
+        }
+        //types 3
+        public static int AppValue;
+        
+        //function that uses/sets types 3 
+        public static void Application(bool isTrue, int num1, int num2)
+        {
+            if (isTrue)
+            {
+                AppValue = num1 + num2;
+            }
+            else
+            {
+                AppValue = num1 - num2;
+            }
+        }
+        
+
+
+
+
+        public static void Main(string[] args)
+        {
+            //sets types
+            Integer();
+            Float();
+            Double();
+            String();
+            Boolean();
+            
+            //writes types
+            Console.WriteLine(MyInteger);
+            Console.WriteLine(MyFloat);
+            Console.WriteLine(MyDouble);
+            Console.WriteLine(MyString);
+            Console.WriteLine(MyBoolean);
+            
+            //calculates types 2
+            Calculator();
+            
+            //writes types 2
+            Console.WriteLine(ValueInteger);
+            Console.WriteLine(ValueFloat);
+            Console.WriteLine(ValueDouble);
+            Console.WriteLine(ValueString);
+            Console.WriteLine(ValueBoolean);
+            
+            //calls application with isTrue as true, num1 as 1, num2 as 1
+            Application(true, 1, 1);
+            
+            //writes types 3
+            Console.WriteLine(AppValue);
+            
+            //calls application with isTrue as false, num1 as 1, num2 as 1
+            Application(false, 1, 1);
+            
+            //writes types 3
+            Console.WriteLine(AppValue);
+        }
+
+    }
+    
+}
---
+++ PLTW 1.3.2/PLTW 1.2.3/PLTW 1.2.3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PLTW1.2.3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PLTW1.2.3")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("A2F50C4F-1379-4231-8583-AB5B2E79C241")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
